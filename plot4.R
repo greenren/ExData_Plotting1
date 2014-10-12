53b03417e86ab34be87eb3d5794111639ad94b47
greenren <timmykok@gmail.com>
2014-10-13 01:51:33 +0200

    Changes to plot4.R and plot4.png

--- plot4.R
+++ plot4.R
@@ -17,6 +17,22 @@ data$DateTime <- strptime(paste(as.character(data$Date), strftime(data$Time, "%H
 
 #Create plot4
 png(file = "plot4.png")
-with(data, plot(DateTime, Global_active_power, type='l', ylab="Global Active Power (kilowatts)", xlab=""))
+par(mfrow = c(2,2), mar = c(4,4,2,1), oma = c(3,0,3,0))
+
+#1.1
+with(data, plot(DateTime, Global_active_power, type='l', ylab="Global Active Power", xlab=""))
+
+#1.2
+with(data, plot(DateTime, Voltage, type='l'))
+
+#2.1
+with(data, plot(DateTime, Sub_metering_1, type='l', xlab="", ylab="Energy sub metering"))
+with(data, lines(DateTime, Sub_metering_2, type='l', col='red'))
+with(data, lines(DateTime, Sub_metering_3, type='l', col='blue'))
+legend("topright", pch="—", col=c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+#2.2
+with(data, plot(DateTime, Global_reactive_power, type='l'))
+
 
 dev.off()
