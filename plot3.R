9e875722133efd1f140985d0c15ee34a465aa2cf
greenren <timmykok@gmail.com>
2014-10-13 01:39:32 +0200

    Finished plot3.R and plot3.png

--- plot3.R
+++ plot3.R
@@ -17,6 +17,9 @@ data$DateTime <- strptime(paste(as.character(data$Date), strftime(data$Time, "%H
 
 #Create plot3
 png(file = "plot3.png")
-with(data, plot(DateTime, Global_active_power, type='l', ylab="Global Active Power (kilowatts)", xlab=""))
+with(data, plot(DateTime, Sub_metering_1, type='l', xlab="", ylab="Energy sub metering"))
+with(data, lines(DateTime, Sub_metering_2, type='l', col='red'))
+with(data, lines(DateTime, Sub_metering_3, type='l', col='blue'))
+legend("topright", pch='l', col=c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 dev.off()
