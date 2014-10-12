04e41e77620b940ceee4022688fd4f6407121919
greenren <timmykok@gmail.com>
2014-10-13 01:41:39 +0200

    Changes plot3.R

--- plot3.R
+++ plot3.R
@@ -20,6 +20,6 @@ png(file = "plot3.png")
 with(data, plot(DateTime, Sub_metering_1, type='l', xlab="", ylab="Energy sub metering"))
 with(data, lines(DateTime, Sub_metering_2, type='l', col='red'))
 with(data, lines(DateTime, Sub_metering_3, type='l', col='blue'))
-legend("topright", pch='l', col=c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", pch="—", col=c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 dev.off()
